4725c0c5b6284d263afe320cc20ff8094cb05a91
openqubit <qubitsmith@gmail.com>
2016-09-05 06:08:00 +0300

    Update reactive-maps-example.js

--- reactive-maps-example.js
+++ reactive-maps-example.js
@@ -3,8 +3,8 @@ Markers = new Mongo.Collection('markers');
 if(Meteor.isClient) {
     Router.route('/',function(){
         console.log('landing page is loaded');
-        var ran = Math.floor((Math.random() * 544) + 1);
-        document.location.href="/"+ran;
+        var ran = toString(Math.floor((Math.random() * 544) + 1));
+        window.location = '/' + ran;
     });
 }
 
