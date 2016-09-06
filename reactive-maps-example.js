0a329adae624e2bab9f2620d7dfb016f96a2e37a
openqubit <qubitsmith@gmail.com>
2016-09-07 02:26:07 +0300

    Update reactive-maps-example.js

--- reactive-maps-example.js
+++ reactive-maps-example.js
@@ -19,10 +19,66 @@ if (Meteor.isClient) {
 
   });
 
-  
+Template.NotFound.onRendered(function () {
+var myApp = new Framework7();
  
-
-Template.map.onRendered(function() {
+var $$ = Dom7;
+ 
+  myApp.modal({
+    title:  'GeoCanada Login Service',
+    text: 'Login With Any Of The Methods Provided',
+    verticalButtons: true,
+    buttons: [
+      {
+      	title: 'Google',
+        text: '<i class="fa fa-google" aria-hidden="true"> Google</i>',
+        onClick: function() {
+          Meteor.loginWithGoogle({
+        requestPermissions: ['email']
+       }, function(error) {
+      if (error) {
+    console.log(error); //If there is any error, will get error here
+    document.location.reload(true);
+     }else{
+    console.log(Meteor.user());// If there is successful login, you will get login details here
+     }
+     });
+        }
+      },
+      {
+      	title: 'Facebook',
+        text: '<i class="fa fa-facebook-official" aria-hidden="true"> Facebook</i>',
+        onClick: function() {
+           Meteor.loginWithFacebook({
+        requestPermissions: ['email']
+       }, function(error) {
+      if (error) {
+    console.log(error); //If there is any error, will get error here
+    document.location.reload(true);
+     }else{
+    console.log(Meteor.user());// If there is successful login, you will get login details here
+     }
+     });
+        }
+      },
+      {
+      	title: 'Twitter',
+        text: '<i class="fa fa-twitter-square" aria-hidden="true"> Twitter</i>',
+        onClick: function() {
+          Meteor.loginWithTwitter({
+        requestPermissions: ['email']
+       }, function(error) {
+      if (error) {
+    console.log(error); //If there is any error, will get error here
+    document.location.reload(true);
+     }else{
+    console.log(Meteor.user());// If there is successful login, you will get login details here
+     }
+     });
+        }
+      }
+    ]
+  })
 
 });
 }
