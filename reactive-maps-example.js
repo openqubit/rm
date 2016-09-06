d901a15f1ca4cc42834fd646e6096e8f7d26b110
openqubit <qubitsmith@gmail.com>
2016-09-07 02:24:37 +0300

    Update reactive-maps-example.js

--- reactive-maps-example.js
+++ reactive-maps-example.js
@@ -1,12 +1,4 @@
-Markers = new Mongo.Collection('markers');
 
-if(Meteor.isClient) {
-    Router.route('/',function(){
-        console.log('landing page is loaded');
-        var ran = toString(Math.floor((Math.random() * 544) + 1));
-        window.location = '/'+Math.random();
-    });
-}
 
 if (Meteor.isClient) {
   
@@ -16,70 +8,22 @@ if (Meteor.isClient) {
    alert('hello');
   }
    });
+ Template.logout.events({
+'click #logout': function(){
+  Meteor.logout();
+    document.location.reload(true);
+}	
+});
 
   Template.map.onCreated(function() {
 
   });
 
- if (Meteor.isClient) {
-  //var MAP_ZOOM = 15;
-
- Meteor.startup(function() {
-  localStorage.setItem("lastname", "Smith");
-  GoogleMaps.load({
-    key: 'AIzaSyD81kt-LoD3_Vqyqhd1yw9YlHq8J3SHpEg'
-  });
-});
-}
-
-  Template.map.helpers({
-    mapOptions: function() {
-      if (GoogleMaps.loaded()) {
-        return {
-          center: new google.maps.LatLng(-37.8136, 144.9631),
-          zoom: 8
-        };
-      }
-    }
-  });
   
  
 
 Template.map.onRendered(function() {
-         GoogleMaps.ready('map', function(map) {
-      google.maps.event.addListener(map.instance, 'click', function(event) {
-        Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
-      });
-    google.maps.event.trigger(map.instance,'resize');
-map.instance.setZoom(6);
-      var markers = {};
-     google.maps.event.trigger(map.instance, "resize");
-      Markers.find().observe({
-        added: function (document) {
-          var marker = new google.maps.Marker({
-            draggable: true,
-            animation: google.maps.Animation.DROP,
-            position: new google.maps.LatLng(document.lat, document.lng),
-            map: map.instance,
-            id: document._id
-          });
 
-          google.maps.event.addListener(marker, 'dragend', function(event) {
-            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-          });
-
-          markers[document._id] = marker;
-        },
-        changed: function (newDocument, oldDocument) {
-          markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-        },
-        removed: function (oldDocument) {
-          markers[oldDocument._id].setMap(null);
-          google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
-          delete markers[oldDocument._id];
-        }
-      });
-    }); 
 });
 }
 
