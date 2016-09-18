a5c3a24fe58abd34693c9134c640d775916a13ff
openqubit <qubitsmith@gmail.com>
2016-09-18 16:39:34 +0300

    Update reactive-maps-example.js

--- reactive-maps-example.js
+++ reactive-maps-example.js
@@ -60,7 +60,22 @@ if (Meteor.isClient) {
   }
    });
    Template.au.onRendered(function() {
-  alert('onrendered');
+       $('.table > tbody  > tr').each(function() {
+var friendid = $(this).find("td:first").html();
+var userid = Meteor.userId();
+ 
+ var selector = {
+            "userid": cuserid,
+            "friendid":userid
+            };
+
+           var this_exists = Friends.find(selector, {limit: 1}).count() > 0;
+           if(this_exists == true) {
+           alert('true');
+           } else {
+        alert('false');
+           }
+});
 });
     Template.au.events({
   'click .btn': function (event,template) {
