514f208c6d022f75fb5d18e847846344972ecae8
openqubit <qubitsmith@gmail.com>
2016-09-18 23:26:55 +0300

    Update reactive-maps-example.js

--- reactive-maps-example.js
+++ reactive-maps-example.js
@@ -60,7 +60,7 @@ if (Meteor.isClient) {
   }
    });
    Template.au.onRendered(function () {
-       
+       $('td:nth-child(1)').hide();
       });
     Template.au.events({
   'click .af': function (event,template) {
