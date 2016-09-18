8a3c108a623566f84852cfb7a7ad61644e9f6abf
openqubit <qubitsmith@gmail.com>
2016-09-18 19:19:01 +0300

    Update reactive-maps-example.js

--- reactive-maps-example.js
+++ reactive-maps-example.js
@@ -81,9 +81,9 @@ if (Meteor.isClient) {
 
            var this_exists = Friends.find(selector, {limit: 1}).count() > 0;
            if(this_exists == true) {
-           $( "<button class='btn btn-danger'>Remove Friend</button>").appendTo(friendid);
+               $(friendid).append( "<button class='btn btn-danger'>Remove Friend</button>");
            } else {
-         $( "<button class='btn btn-primary'>Add Friend</button>").appendTo(friendid);
+         $(friendid).append( "<button class='btn btn-primary'>Add Friend</button>");
            }
        });
   }
